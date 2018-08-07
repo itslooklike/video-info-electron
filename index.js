ba3bec9a22eea78db045c7dec3cd6fa00de12473
Alexey Masalov <delsol@bk.ru>
2018-08-08 01:54:08 +0300

    init

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const path = require('path');
 const electron = require('electron');
-const CustomTray = require('./src/custom-tray');
-const MainWindow = require('./src/main-window');
-const { app, ipcMain } = electron;
+const { app, BrowserWindow } = electron;
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
-  app.dock.hide();
-  mainWindow = new MainWindow();
-  const iconPath = path.join(__dirname, './src/assets/tray-icon.png');
-
-  // тут можно не сохранять в переменную, НО тогда ее вычистит GC
-  tray = new CustomTray(iconPath, mainWindow);
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    webPreferences: {
+      backgroundThrottling: false,
+    },
+  });
 });
-
-ipcMain.on('timer:tick', (evt, data) => {
-  tray.setTitle(data.toString());
-});
-
-ipcMain.on('timer:end', () => tray.setTitle(''));
